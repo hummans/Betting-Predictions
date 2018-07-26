7d23958915301f10278484f06d19458e2f1564b4
Yasen <yasen9977@gmail.com>
2018-07-26 16:14:33 +0300

    Initial commit

---
+++ src/main/java/com/teamproject/bet4life/Bet4lifeApplication.java
@@ -0,0 +1,12 @@
+package com.teamproject.bet4life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bet4lifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bet4lifeApplication.class, args);
+    }
+}
