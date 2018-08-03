cc1039463fe039fff3bd1add6bfdec7674b0b2dc
PenyoKolev <penyokolev@gmail.com>
2018-08-03 23:38:07 +0300

    Add BCrypt password encoder

--- src/main/java/com/teamproject/bet4life/configuration/SpringSecurityConfig.java
+++ src/main/java/com/teamproject/bet4life/configuration/SpringSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.teamproject.bet4life.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +22,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/teamproject/bet4life/services/UserServiceImpl.java
+++ src/main/java/com/teamproject/bet4life/services/UserServiceImpl.java
@@ -5,12 +5,16 @@ import com.teamproject.bet4life.model.RegistrationModel;
 import com.teamproject.bet4life.repositories.base.UserRepository;
 import com.teamproject.bet4life.services.base.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public void register(RegistrationModel registrationModel) {
